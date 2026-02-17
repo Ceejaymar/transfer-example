3d4dafd6cd60a8e7048d6540b88e63775d5d8534
ceejaymar <martinezcj2@gmail.com>
2026-02-17 11:27:43 -0500

    fix amount input

--- src/components/amount-input.tsx
+++ src/components/amount-input.tsx
@@ -4,15 +4,20 @@ type AmountInput = {
 };
 
 function AmountInput({ amount, onChange }: AmountInput) {
+	const value = amount === '0' ? '' : amount;
+	const width = value.length === 0 ? '1ch' : `${value.length + 0.5}ch`;
+
 	return (
-		<div className="flex items-center">
-			<span className="text-3xl text-white font-bold">$</span>
+		<div className="flex items-center justify-center w-full gap-1">
+			<span className="text-4xl text-white font-bold">$</span>
+
 			<input
-				className="no-spinner bg-transparent text-white text-center text-5xl font-bold outline-none border-none p-4 caret-blue-500 transition-all"
+				style={{ width }}
+				className="no-spinner bg-transparent text-white text-left text-5xl font-bold outline-none border-none p-0 caret-blue-500 transition-all min-w-[1ch]"
 				type="number"
 				inputMode="decimal"
 				step="0.01"
-				value={amount === '0' ? '' : amount}
+				value={value}
 				onChange={(e) => onChange(e.target.value)}
 				placeholder="0"
 			/>
